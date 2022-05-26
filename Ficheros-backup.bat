213681162a3553073efcc9f7184c5b814514b103
Osuuunaa <100867734+Osuuunaa@users.noreply.github.com>
2022-05-27 01:14:45 +0200

    Delete Ficheros-backup.bat

--- Ficheros-backup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set/p x="Pulse 'y' si quiere clonar los tres archivos: "
-:while
-if %x%== y copy /y FicheroTemperatura.txt CopiaTemperatura.txt && copy /y FicheroLuminosidad.txt CopiaLuminosidad.txt && copy /y FicheroHumedad.txt CopiaHumedad.txt
-timeout /t 1
-goto :while
-pause
